32c5e1d576b51078a78d0aed5cddea6684b21298
Marat <petrosianmav@gmail.com>
2025-08-04 20:38:46 +0300

    Homework-6

--- payment-service/payment-service-app/src/main/java/com/iprody/payment/service/controller/PaymentController.java
+++ payment-service/payment-service-app/src/main/java/com/iprody/payment/service/controller/PaymentController.java
@@ -3,26 +3,29 @@ package com.iprody.payment.service.controller;
 import com.iprody.payment.service.persistence.PaymentRepository;
 import com.iprody.payment.service.persistence.entity.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
 @RestController
 @RequestMapping("/payments")
 public class PaymentController {
+
     @Autowired
     private PaymentRepository paymentRepository;
 
-    @GetMapping("/all")
+    // Получение всех платежей: http://localhost:8088/payments
+    @GetMapping
     public List<Payment> getAll() {
         return paymentRepository.findAll();
     }
 
-    @GetMapping("/guid")
-    public Payment getByID(@PathVariable UUID guid) {
-        return new Payment();
+    // Получение платежа по guid: http://localhost:8088/payments/{guid}
+    @GetMapping("/{guid}")
+    public ResponseEntity<Payment> getByID(@PathVariable UUID guid) {
+        return paymentRepository.findById(guid)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
