71b8902b52e999cb63fb6c5ccd07873701f06da8
Marat <petrosianmav@gmail.com>
2025-08-24 20:19:39 +0300

    методы в классе PCN доработанны

--- payment-service/payment-service-app/src/main/java/com/iprody/payment/service/controller/PaymentControllerNew.java
+++ payment-service/payment-service-app/src/main/java/com/iprody/payment/service/controller/PaymentControllerNew.java
@@ -3,6 +3,7 @@ package com.iprody.payment.service.controller;
 import com.iprody.payment.service.dto.PaymentDto;
 import com.iprody.payment.service.persistence.PaymentFilter;
 import com.iprody.payment.service.services.PaymentService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -21,34 +22,58 @@ import java.util.UUID;
 @RequestMapping("/payments")
 public class PaymentControllerNew {
 
-    private PaymentService paymentService;
+    private final PaymentService paymentService;
 
-    public void PaymentController(PaymentService paymentService) {
+    public PaymentControllerNew(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
 
+    /**
+     * Создание нового платежа
+     */
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.CREATED) // 201
     public PaymentDto create(@RequestBody PaymentDto dto) {
         return paymentService.create(dto);
     }
 
+    /**
+     * Получение платежа по ID
+     */
     @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK) // 200
     public PaymentDto get(@PathVariable UUID id) {
-        return paymentService.get(id);
+        try {
+            return paymentService.get(id);
+        } catch (IllegalArgumentException e) {
+            throw new EntityNotFoundException("Платеж не найден: " + id);
+        }
     }
 
+    /**
+     * Поиск с фильтрацией, сортировкой и пагинацией
+     */
     @GetMapping
+    @ResponseStatus(HttpStatus.OK) // 200
     public Page<PaymentDto> search(PaymentFilter filter, Pageable pageable) {
         return paymentService.search(filter, pageable);
     }
+
+    /**
+     * Обновление платежа
+     */
     @PutMapping("/{id}")
-    public PaymentDto update(@PathVariable UUID id, @RequestBody
-    PaymentDto dto) {
+    @ResponseStatus(HttpStatus.OK) // 200
+    public PaymentDto update(@PathVariable UUID id,
+                             @RequestBody PaymentDto dto) {
         return paymentService.update(id, dto);
     }
+
+    /**
+     * Удаление платежа
+     */
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseStatus(HttpStatus.NO_CONTENT) // 204
     public void delete(@PathVariable UUID id) {
         paymentService.delete(id);
     }
