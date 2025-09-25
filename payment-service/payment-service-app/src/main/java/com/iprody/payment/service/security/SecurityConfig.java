7b0a452376d9209763d22540f126c91db947a6fd
Marat <petrosianmav@gmail.com>
2025-09-25 19:57:55 +0300

    Lesson 13

---
+++ payment-service/payment-service-app/src/main/java/com/iprody/payment/service/security/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.iprody.payment.service.security;
+
+import com.iprody.payment.service.security.KeycloakRealmRoleConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/payments/**").hasRole("user")
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt
+                        (jwt -> jwt.jwtAuthenticationConverter(jwtConverter))
+                );
+
+        return http.build();
+    }
+}
