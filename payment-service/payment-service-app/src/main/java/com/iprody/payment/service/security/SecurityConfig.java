0bec04a5f778d6aa90841136eb1770b8cf7f91d1
Marat <petrosianmav@gmail.com>
2025-09-27 13:19:47 +0300

    Lesson 13. Изменил SecurityConfig

--- payment-service/payment-service-app/src/main/java/com/iprody/payment/service/security/SecurityConfig.java
+++ payment-service/payment-service-app/src/main/java/com/iprody/payment/service/security/SecurityConfig.java
@@ -22,14 +22,17 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/payments/**").hasRole("user")
+                        // доступ и для user, и для admin
+                        .requestMatchers("/payments/**").hasAnyRole("user", "admin")
+                        // доступ только для admin (пример)
+                        .requestMatchers("/admin/**").hasRole("admin")
+                        // всё остальное требует просто авторизацию
                         .anyRequest().authenticated()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt
-                        (jwt -> jwt.jwtAuthenticationConverter(jwtConverter))
-                );
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(
+                        jwt -> jwt.jwtAuthenticationConverter(jwtConverter)
+                ));
 
         return http.build();
     }
